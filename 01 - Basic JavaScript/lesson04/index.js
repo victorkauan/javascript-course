274f33e10db7045ca5f8ee76c03946f0a201fe66
Victor Kauan <victorkauan.dev@gmail.com>
2022-11-08 12:05:05 -0300

    feat: do lesson 04

---
+++ 01 - Basic JavaScript/lesson04/index.js
@@ -0,0 +1,23 @@
+let customerName = 'Victor';
+
+console.log(customerName, 'was born in 1984.');
+console.log('In 2000', customerName, 'met Maria.');
+console.log(customerName, 'married Maria in 2012.');
+console.log('Maria had a child with', customerName, 'in 2015.');
+console.log(customerName, "'s son is called Eduardo.");
+
+let test; // Declare variable
+test = 'Victor'; // Initialize variable
+console.log(test);
+test = 'Kauan'; // Change variable value
+console.log(test);
+
+// RULES AND TIPS FOR CREATING VARIABLES:
+// - Do not use var, use let;
+// - To name variables, use camelCase;
+// - Variables cannot have reserved names;
+// - Variables must have meaningful names;
+// - Variables cannot be redeclared with let;
+// - Variable names cannot start with a number;
+// - Variable names cannot contain spaces or dashes;
+// - Variables are case-sensitive (there is a distinction between uppercase and lowercase letters).
---
+++ 01 - Basic JavaScript/lesson04/tests.js
@@ -0,0 +1,5 @@
+console.log('João was born in 1984.');
+console.log('In 2000 João met Maria.');
+console.log('João married Maria in 2012.');
+console.log('Maria had a child with João em 2015.');
+console.log("João's son is called Eduardo.");
