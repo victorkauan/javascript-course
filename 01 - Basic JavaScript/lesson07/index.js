d0a05ad35031896e950e1e88f4efc28d08ca100f
Victor Kauan <victorkauan.dev@gmail.com>
2022-11-08 14:27:40 -0300

    feat: do lesson 07

---
+++ 01 - Basic JavaScript/lesson07/index.js
@@ -0,0 +1,10 @@
+var varName = 'Victor';
+var varName = 'Kauan';
+console.log(varName);
+
+let letName = 'Victor';
+// let letName = 'Kauan'; // [ERROR]
+console.log(letName);
+
+test = 10; // Do not do that, always use let or const
+console.log(test);
