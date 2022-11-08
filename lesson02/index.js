57c2a939677b490c8a17070394d52b3655d348be
Victor Kauan <victorkauan.dev@gmail.com>
2022-11-08 11:04:48 -0300

    feat: do lesson 02

---
+++ lesson02/index.js
@@ -0,0 +1,18 @@
+// Write a comment here
+console.log('Hello, world!'); // Here's another comment
+console.log('Line 01');
+
+/*
+// console.log('Line 02');
+  console.log('Line 03');
+  console.log('Line 04');
+*/
+
+/*
+  Code block
+  - Comment line
+  - Comment line
+  - Comment line
+*/
+
+// This comment will be ignored
