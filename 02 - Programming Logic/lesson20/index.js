3089a15b8463c12b961870e4d627f1f3102893e8
Victor Kauan <victorkauan.dev@gmail.com>
2023-01-02 21:10:47 -0300

    feat: create programming logic module directory and do lesson 20

---
+++ 02 - Programming Logic/lesson20/index.js
@@ -0,0 +1,70 @@
+/*
+  Comparison operators
+  - Greater than: >
+  - Greater than or equal to: >=
+  - Less than: <
+  - Less than or equal to: <=
+  - Equal to (value): ==
+  - Equal value and equal type: ===
+  - Not equal (value): !=
+  - Not equal value or not equal type: !==
+*/
+
+const number1 = 10;
+const number2 = 5;
+
+const comparison = number1 > number2;
+
+console.log(comparison);
+
+console.log();
+
+console.log(10 > 9);
+console.log(10 > 10);
+console.log(10 > 11);
+
+console.log();
+
+console.log(10 >= 9);
+console.log(10 >= 10);
+console.log(10 >= 11);
+
+console.log();
+
+console.log(10 < 9);
+console.log(10 < 10);
+console.log(10 < 11);
+
+console.log();
+
+console.log(10 <= 9);
+console.log(10 <= 10);
+console.log(10 <= 11);
+
+console.log();
+
+console.log(10 == 10);
+console.log(10 == 11);
+console.log(10 == '10');
+console.log(10 == '11');
+
+console.log();
+
+console.log(10 === 10);
+console.log(10 === 11);
+console.log(10 === '10');
+console.log(10 === '11');
+
+console.log();
+
+console.log(10 != 10);
+console.log(10 != 11);
+console.log(10 != '10');
+console.log(10 != '11');
+
+console.log();
+
+console.log(10 !== 10);
+console.log(10 !== 11);
+console.log(10 !== '10');
+console.log(10 !== '11');
