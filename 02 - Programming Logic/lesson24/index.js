ae80b882ecc60787059d56062693b6bec7c9d0a7
Victor Kauan <victorkauan.dev@gmail.com>
2023-01-04 23:02:07 -0300

    feat: do lesson 24

---
+++ 02 - Programming Logic/lesson24/index.js
@@ -0,0 +1,52 @@
+const number = 10;
+
+if (number >= 0 && number <= 5) {
+  console.log('The number is between 0 and 5.');
+} else {
+  console.log("The number isn't between 0 and 5.");
+}
+
+console.log();
+
+if (number >= 0 && number <= 5) {
+  console.log('The number is between 0 and 5.');
+} else if (number >= 6 && number <= 8) {
+  console.log('The number is between 6 and 8.');
+} else if (number >= 9 && number <= 11) {
+  console.log('The number is between 9 and 11.');
+} else {
+  console.log("The number isn't between 0 and 11.");
+}
+
+console.log();
+
+if (number >= 0 && number <= 5) {
+  console.log('The number is between 0 and 5.');
+} else if (number >= 6 && number <= 8) {
+  console.log('The number is between 6 and 8.');
+} else if (1 === 1) {
+  console.log('Literal.');
+} else if (number >= 9 && number <= 11) {
+  console.log('The number is between 9 and 11.');
+} else {
+  console.log("The number isn't between 0 and 11.");
+}
+
+console.log();
+
+if (number >= 0 && number <= 5) {
+  console.log('The number is between 0 and 5.');
+} else if (number >= 6 && number <= 8) {
+  console.log('The number is between 6 and 8.');
+} else if (number >= 9 && number <= 11) {
+  console.log('The number is between 9 and 11.');
+} else {
+  console.log("The number isn't between 0 and 11.");
+}
+
+if (number <= 10) {
+  console.log('The number is less than or equal to 10.');
+}
+
+console.log();
+console.log('More code...');
